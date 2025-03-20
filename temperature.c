84b6542ae4d9754664d1b6c44ce89b7c51a30251
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-20 18:34:11 -0500

    added kel_to_fah() and fah_to_kel() functions

--- temperature.c
+++ temperature.c
@@ -19,3 +19,13 @@ float kelvin_to_celsius(float kelvin){
     float result = kelvin - 273.15;
     return result;
 }
+
+float kelvin_to_fahrenheit(float kelvin){
+    float result = (kelvin - 273.15) * (9.0 / 5.0) + 32;
+    return result;
+}
+
+float fahrenheit_to_kelvin(float fahrenheit){
+    float result = (fahrenheit - 32) * (5.0 / 9.0) + 273.15;
+    return result;
+}
