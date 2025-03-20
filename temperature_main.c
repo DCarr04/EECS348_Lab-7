a913664950eb56bd56eed3d0a3f46d15a7255bc3
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-19 21:28:14 -0500

    code to convert from celsius to fahrenheit

--- temperature_main.c
+++ temperature_main.c
@@ -1,5 +1,26 @@
 #include "temperature.h"
 
 int main(int argc, char *argv[]) {
+    float userTemp;
+    int currentScale;
+    int convertScale;
+    float *uTemp;
+    uTemp = &userTemp;
+    printf("Enter the temperature: ");
+    scanf("%f", &userTemp);
+    printf("Choose the current scale (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
+    scanf("%d", &currentScale);
+    printf("Convert to (1) Celsius, (2) Fahrenheit, (3) Kelvin: ");
+    scanf("%d", &convertScale);
+
+    if(currentScale == 1 && convertScale == 2){
+        float converted = celsius_to_fahrenheit(*uTemp);
+        printf("%f\n", converted);
+    }else{
+        printf("Error, cannot convert between these scales");
+    }
+    
+
+    
     return 0;
 }
