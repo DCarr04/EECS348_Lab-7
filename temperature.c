3212a0eb4bb3bca9ebc3bb30a4fe7b6392bb8517
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-19 23:07:15 -0500

    fixed issue by changing 9/5 to 9.0/5.0

--- temperature.c
+++ temperature.c
@@ -1,7 +1,6 @@
 #include "temperature.h"
 
 float celsius_to_fahrenheit(float celsius){
-    float ratio = 9 / 5;
-    float result = (ratio * celsius) + 32;
+    float result = ((9.0 / 5.0) * celsius) + 32;
     return result;
 }
