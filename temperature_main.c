a20928c00f308377f4dbaac5d1d416e83f38bfbb
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-20 20:07:06 -0500

    added categorize_temp() call with toCelsius as parameter

--- temperature_main.c
+++ temperature_main.c
@@ -29,6 +29,9 @@ int main(int argc, char *argv[]) {
     }else if(currentScale == 2 && convertScale == 3){
         float converted = fahrenheit_to_kelvin(*uTemp); //try other method
         printf("Converted temperature: %f\n", converted);
+
+        float toCelsius = fahrenheit_to_celsius(*uTemp);
+        categorize_temperature(toCelsius);
     }else if(currentScale == 3 && convertScale == 1){
         float converted = kelvin_to_celsius(*uTemp);
         printf("Converted temperature: %f\n", converted);
@@ -36,6 +39,9 @@ int main(int argc, char *argv[]) {
     }else if(currentScale == 3 && convertScale == 2){
         float converted = kelvin_to_fahrenheit(*uTemp); //try other method
         printf("Converted temperature: %f\n", converted);
+
+        float toCelsius = kelvin_to_celsius(*uTemp);
+        categorize_temperature(toCelsius);
     }else{
         printf("Error, cannot convert between these scales");
     }
