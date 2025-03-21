576aae66526ff79b25b30c1db60b352b193890f3
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-20 20:47:52 -0500

    added measurement units

--- temperature_main.c
+++ temperature_main.c
@@ -16,29 +16,29 @@ int main(int argc, char *argv[]) {
 
     if(currentScale == 1 && convertScale == 2){
         float converted = celsius_to_fahrenheit(*uTemp);
-        printf("Converted temperature: %f\n", converted);
+        printf("Converted temperature: %f°F\n", converted);
         categorize_temperature(*uTemp);
     }else if(currentScale == 1 && convertScale == 3){
         float converted = celsius_to_kelvin(*uTemp);
-        printf("Converted temperature: %f\n", converted);
+        printf("Converted temperature: %fK\n", converted);
         categorize_temperature(*uTemp);
     }else if(currentScale == 2 && convertScale == 1){
         float converted = fahrenheit_to_celsius(*uTemp);
-        printf("Converted temperature: %f\n", converted);
+        printf("Converted temperature: %f°C\n", converted);
         categorize_temperature(converted);
     }else if(currentScale == 2 && convertScale == 3){
         float converted = fahrenheit_to_kelvin(*uTemp); //try other method
-        printf("Converted temperature: %f\n", converted);
+        printf("Converted temperature: %fK\n", converted);
 
         float toCelsius = fahrenheit_to_celsius(*uTemp);
         categorize_temperature(toCelsius);
     }else if(currentScale == 3 && convertScale == 1){
         float converted = kelvin_to_celsius(*uTemp);
-        printf("Converted temperature: %f\n", converted);
+        printf("Converted temperature: %f°C\n", converted);
         categorize_temperature(converted);
     }else if(currentScale == 3 && convertScale == 2){
         float converted = kelvin_to_fahrenheit(*uTemp); //try other method
-        printf("Converted temperature: %f\n", converted);
+        printf("Converted temperature: %f°F\n", converted);
 
         float toCelsius = kelvin_to_celsius(*uTemp);
         categorize_temperature(toCelsius);
