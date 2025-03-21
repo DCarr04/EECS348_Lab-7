90f443a874f5a2128f658983a60b2fe0f2ab424c
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-20 20:03:28 -0500

    categorize_temp() calls with converted as parameter added

--- temperature_main.c
+++ temperature_main.c
@@ -25,12 +25,14 @@ int main(int argc, char *argv[]) {
     }else if(currentScale == 2 && convertScale == 1){
         float converted = fahrenheit_to_celsius(*uTemp);
         printf("Converted temperature: %f\n", converted);
+        categorize_temperature(converted);
     }else if(currentScale == 2 && convertScale == 3){
         float converted = fahrenheit_to_kelvin(*uTemp); //try other method
         printf("Converted temperature: %f\n", converted);
     }else if(currentScale == 3 && convertScale == 1){
         float converted = kelvin_to_celsius(*uTemp);
         printf("Converted temperature: %f\n", converted);
+        categorize_temperature(converted);
     }else if(currentScale == 3 && convertScale == 2){
         float converted = kelvin_to_fahrenheit(*uTemp); //try other method
         printf("Converted temperature: %f\n", converted);
