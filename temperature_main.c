35c8f92942d003ecf43dd88c960e29e051139f2a
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-20 20:01:56 -0500

    added function calls for conversion functions in the if statements

--- temperature_main.c
+++ temperature_main.c
@@ -17,6 +17,23 @@ int main(int argc, char *argv[]) {
     if(currentScale == 1 && convertScale == 2){
         float converted = celsius_to_fahrenheit(*uTemp);
         printf("Converted temperature: %f\n", converted);
+        categorize_temperature(*uTemp);
+    }else if(currentScale == 1 && convertScale == 3){
+        float converted = celsius_to_kelvin(*uTemp);
+        printf("Converted temperature: %f\n", converted);
+        categorize_temperature(*uTemp);
+    }else if(currentScale == 2 && convertScale == 1){
+        float converted = fahrenheit_to_celsius(*uTemp);
+        printf("Converted temperature: %f\n", converted);
+    }else if(currentScale == 2 && convertScale == 3){
+        float converted = fahrenheit_to_kelvin(*uTemp); //try other method
+        printf("Converted temperature: %f\n", converted);
+    }else if(currentScale == 3 && convertScale == 1){
+        float converted = kelvin_to_celsius(*uTemp);
+        printf("Converted temperature: %f\n", converted);
+    }else if(currentScale == 3 && convertScale == 2){
+        float converted = kelvin_to_fahrenheit(*uTemp); //try other method
+        printf("Converted temperature: %f\n", converted);
     }else{
         printf("Error, cannot convert between these scales");
     }
