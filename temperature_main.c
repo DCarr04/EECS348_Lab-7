28fcfd38c687c8c9c6e30ddf12628932e3b2e3ef
Didier Carrillo <didiercarrillo40@gmail.com>
2025-03-20 22:09:03 -0500

    modified fah_to_kel in function call

--- temperature_main.c
+++ temperature_main.c
@@ -18,30 +18,41 @@ int main(int argc, char *argv[]) {
         float converted = celsius_to_fahrenheit(*uTemp);
         printf("Converted temperature: %f°F\n", converted);
         categorize_temperature(*uTemp);
+
     }else if(currentScale == 1 && convertScale == 3){
         float converted = celsius_to_kelvin(*uTemp);
         printf("Converted temperature: %fK\n", converted);
         categorize_temperature(*uTemp);
+
     }else if(currentScale == 2 && convertScale == 1){
         float converted = fahrenheit_to_celsius(*uTemp);
         printf("Converted temperature: %f°C\n", converted);
         categorize_temperature(converted);
+
     }else if(currentScale == 2 && convertScale == 3){
+        /*
         float converted = fahrenheit_to_kelvin(*uTemp); //try other method
         printf("Converted temperature: %fK\n", converted);
+        */
+        float fah_to_cel = fahrenheit_to_celsius(*uTemp);
+        float cel_to_kel = celsius_to_kelvin(fah_to_cel);
+        printf("Converted temperature: %fK\n", cel_to_kel);
 
         float toCelsius = fahrenheit_to_celsius(*uTemp);
         categorize_temperature(toCelsius);
+
     }else if(currentScale == 3 && convertScale == 1){
         float converted = kelvin_to_celsius(*uTemp);
         printf("Converted temperature: %f°C\n", converted);
         categorize_temperature(converted);
+
     }else if(currentScale == 3 && convertScale == 2){
         float converted = kelvin_to_fahrenheit(*uTemp); //try other method
         printf("Converted temperature: %f°F\n", converted);
 
         float toCelsius = kelvin_to_celsius(*uTemp);
         categorize_temperature(toCelsius);
+
     }else{
         printf("Error, cannot convert between these scales");
     }
